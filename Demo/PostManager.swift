5458441916239644ed2dea76bd8d69af39017da6
Roy Hsu <roy.hsu@tinyworld.cc>
2018-05-17 19:52:43 +0800

    inject the session dependency.

--- Demo/PostManager.swift
+++ Demo/PostManager.swift
@@ -12,6 +12,10 @@ import Foundation
 
 class PostManager: PostProvider {
     
+    let session: URLSession
+    
+    init(session: URLSession = .shared) { self.session = session }
+    
     enum FetchPostError: Error {
         
         case noData
@@ -25,7 +29,7 @@ class PostManager: PostProvider {
         
         let url = URL(string: "https://jsonplaceholder.typicode.com/posts/\(id)")!
         
-        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+        let task = session.dataTask(with: url) { data, response, error in
             
             if let error = error {
                 
